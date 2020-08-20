a89d76b4f7d72192f963074c08298fd8656cd859
DenisViskov <DenisViskov16@gmail.com>
2020-08-20 14:36:36 +0300

    added redirect on .do in Post

--- src/main/java/servlet/PostServlet.java
+++ src/main/java/servlet/PostServlet.java
@@ -19,6 +19,13 @@ import java.time.LocalDateTime;
  * @since 19.08.2020
  */
 public class PostServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("posts", Store.instOf().findAllPosts());
+        req.getRequestDispatcher("posts.jsp").forward(req, resp);
+    }
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setCharacterEncoding("UTF-8");
@@ -30,6 +37,6 @@ public class PostServlet extends HttpServlet {
                         LocalDateTime.now()
                 )
         );
-        resp.sendRedirect(req.getContextPath() + "/post/posts.jsp");
+        resp.sendRedirect(req.getContextPath() + "/post/posts.do");
     }
 }
