40cc56350e9a580a9aaabb5a36e89744ca7c14bf
DenisViskov <denisviskov16@gmail.com>
2020-08-27 15:13:43 +0300

    update

---
+++ src/main/java/model/User.java
@@ -0,0 +1,74 @@
+package model;
+
+import java.util.Objects;
+
+/**
+ * CLass is an User
+ *
+ * @author Денис Висков
+ * @version 1.0
+ * @since 27.08.2020
+ */
+public class User {
+    /**
+     * ID
+     */
+    private int id;
+    /**
+     * Name
+     */
+    private String name;
+    /**
+     * Email
+     */
+    private String email;
+    /**
+     * Password
+     */
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
