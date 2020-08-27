2928e16343d289d649a094c45cda7f6451030318
DenisViskov <denisviskov16@gmail.com>
2020-08-27 16:27:46 +0300

    2. Filter [#334583]

---
+++ src/main/java/filter/AuthFilter.java
@@ -0,0 +1,41 @@
+package filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Class is a Filter session
+ *
+ * @author Денис Висков
+ * @version 1.0
+ * @since 27.08.2020
+ */
+public class AuthFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse resp = (HttpServletResponse) servletResponse;
+        String uri = req.getRequestURI();
+        if (uri.endsWith("auth.do")) {
+            filterChain.doFilter(servletRequest, servletResponse);
+            return;
+        }
+        if (req.getSession().getAttribute("user") == null) {
+            resp.sendRedirect(req.getContextPath() + "/login/login.jsp");
+            return;
+        }
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
