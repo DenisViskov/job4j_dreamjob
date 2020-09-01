470626ea01e3aca1d1ec871bfe9a420f542a259a
DenisViskov <denisviskov16@gmail.com>
2020-09-01 10:53:17 +0300

    4. Ajax Jquery [#334551]

---
+++ src/main/java/servlet/GreetingServlet.java
@@ -0,0 +1,27 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Class is a Greeting servlet
+ *
+ * @author Денис Висков
+ * @version 1.0
+ * @since 31.08.2020
+ */
+public class GreetingServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/plain");
+        resp.setCharacterEncoding("UTF-8");
+        String name = req.getParameter("name");
+        PrintWriter writer = new PrintWriter(resp.getOutputStream());
+        writer.println("Nice to meet you, " + name);
+        writer.flush();
+    }
+}
